83433ef4437037a8c1e831199fc4caa52b390d5a
Masoud Zohrabi <mdzzohrabi@gmail.com>
2022-11-30 11:52:43 +0330

    update

--- src-ui/pages/logs.jsx
+++ src-ui/pages/logs.jsx
@@ -8,6 +8,7 @@ import { Container } from "../components/container";
 import { io } from "socket.io-client";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
+import { useCallback } from "react";
 
 export default function LogPage() {
 
@@ -27,7 +28,7 @@ export default function LogPage() {
         socket?.on('log', (log) => setLogs([...logs, log]));
     }, [logs, setLogs, socket, setConnected]);
 
-    useEffect(() => {
+    let emitFilter = useCallback(() => {
         socket?.emit('filter', filter);
     }, [socket, filter]);
 
@@ -50,6 +51,9 @@ export default function LogPage() {
                 <label htmlFor="filter" className={"py-1 pr-2 self-start font-semibold"}>Filter</label>                
                 <input type={"text"} id="filter" className="border-gray-500 border-solid border-b-0 bg-slate-100 rounded-md invalid:border-red-500 invalid:ring-red-600 px-2 py-1 focus:outline-blue-500" onChange={e => setFilter(e.currentTarget.value)} value={filter}/>
             </div>
+            <button className="bg-slate-200 px-5 py-1 rounded-lg text-sm hover:bg-slate-800 hover:text-white hover:shadow-md duration-75" onClick={() => emitFilter()}>
+                Set Filter
+            </button>
             <button className="bg-slate-200 px-5 py-1 rounded-lg text-sm hover:bg-slate-800 hover:text-white hover:shadow-md duration-75" onClick={() => setLogs([])}>
                 Clear Logs
             </button>
@@ -64,7 +68,7 @@ export default function LogPage() {
                 <tr>
                     <th colSpan={2} className={classNames('py-1', { 'bg-green-300': isConnected, 'bg-red-300': !isConnected })}>{isConnected ? 'Connected' : 'Disconnected'}</th>
                 </tr>
-                {logs.filter(x => !filter || x.includes(filter)).map((log, index) => {
+                {logs.map((log, index) => {
                     return <tr key={index}>
                         <td>{log}</td>
                     </tr>;
