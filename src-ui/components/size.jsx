957e4c29c3d98c68b685d9f8469876baf9bcb44b
Masoud Zohrabi <mdzzohrabi@gmail.com>
2022-12-28 07:22:11 +0330

    update

---
+++ src-ui/components/size.jsx
@@ -0,0 +1,17 @@
+/**
+ * Size
+ * @param {{ size: number }} param0 
+ * @returns 
+ */
+export function Size({ size }) {
+    if (!size) return <>{size}</>;
+
+    let postfix = ['B', 'KB', 'MB', 'GB', 'TB', 'EB'];
+    let index = 0;
+    while (size > 1024) {
+        size = size / 1024;
+        index++;
+    }
+
+    return <>{Math.round(size)} {postfix[index]}</>;
+}
--- src-ui/pages/usages/traffic.jsx
+++ src-ui/pages/usages/traffic.jsx
@@ -9,6 +9,7 @@ import { AppContext } from "../../components/app-context";
 import { Container } from "../../components/container";
 import { Field, FieldsGroup } from "../../components/fields";
 import { Info, Infos } from "../../components/info";
+import { Size } from "../../components/size";
 import { Table } from "../../components/table";
 import { usePrompt, useStoredState } from "../../lib/hooks";
 import { styles } from "../../lib/styles";
@@ -21,7 +22,12 @@ export default function TrafficUsagePage() {
     let showAll = router.query.all == '1';
     let email = router.query.user;
     let [view, setView] = useStoredState('usages-traffic-view', {
-        showDetail: showAll ? true : false
+        showDetail: showAll ? true : false,
+        sortColumn: 'type',
+        sortAsc: true,
+        filter: '',
+        direction: '',
+        type: ''
     });
 
     /**
@@ -38,17 +44,71 @@ export default function TrafficUsagePage() {
             { email ? <Field label="User" className="border-x-[1px] px-3 mr-2">
                 <span className="text-gray-800 py-1 px-2 rounded-lg bg-yellow-100">{email}</span>
             </Field> : null }
+            <Field label="Sort" htmlFor="sortColumn">
+                <select id="sortColumn" className={styles.input}>
+                    <option value="-">-</option>
+                    <option value="type">Type</option>
+                    <option value="name">Name</option>
+                    <option value="direction">Direction</option>
+                    <option value="traffic">Traffic</option>
+                </select>
+            </Field>
+            <Field label="Order" htmlFor="sort-order">
+                <select value={view?.sortAsc ? "asc" : "desc"} id="sort-order" className={styles.input} onChange={e => setView({ ...view, sortAsc: e.currentTarget.value == "asc" })}>
+                    <option value={"asc"}>ASC</option>
+                    <option value={"desc"}>DESC</option>
+                </select>
+            </Field>
+            <Field label="Direction" htmlFor="direction">
+                <select id="direction" className={styles.input}>
+                    <option value={''}>-</option>
+                    <option value="uplink">UpLink</option>
+                    <option value="downlink">DownLink</option>
+                </select>
+            </Field>
+            <Field label="Type" htmlFor="type">
+                <select id="type" className={styles.input}>
+                    <option value={''}>-</option>
+                    <option value="user">User</option>
+                    <option value="outbound">Outbound</option>
+                    <option value="inbound">Inbound</option>
+                </select>
+            </Field>
+            <Field label="Filter" htmlFor="filter">
+                <input type={"text"} id="filter" className={styles.input}/>
+            </Field>
+            {/* <Field label="Status" htmlFor="status">
+                <div className="flex gap-1 mb-1">
+                    {view.statusFilter?.map((filter, index) => <span key={index} onClick={() => setView({ ...view, statusFilter: view.statusFilter.filter(x => x != filter)})} className={classNames("whitespace-nowrap bg-slate-200 px-3 py-1 rounded-3xl cursor-pointer hover:bg-slate-700 hover:text-white")}>{filter}</span> )}
+                </div>
+                <select value={"-"} onChange={e => setView({ ...view, statusFilter: [...view.statusFilter, e.currentTarget.value]})} id="statusFilter" className="bg-slate-100 rounded-lg px-2 py-1">
+                    <option value="-">-</option>
+                    {(statusFilters ?? []).map((x, index) => <option key={index} value={x}>{x}</option>)}
+                </select>
+            </Field> */}
+            <div className="flex flex-row">
+                {showAll ? <button className={styles.button} onClick={() => refreshUsages()}>Reload</button> : null }
+            </div>
         </FieldsGroup>
+        {isLoading ? <div className="absolute bg-slate-900 text-white rounded-lg px-3 py-1 bottom-3 left-3">
+            Loading ...
+        </div> : null }
         <Table
-            rows={Object.keys(usages ?? {}).flatMap(date => [ { date, group: true }, ...usages[date] ])}
+            rows={Object.keys(usages ?? {}).flatMap(date => [ { date, group: true }, ...(
+                usages[date]
+                    .sort(arrSort(view.sortColumn, view.sortAsc))
+                    .filter(x => view.filter ? x.name.includes(view.filter) : true)
+                    .filter(x => !!view.direction ? x.direction == view.direction : true)
+                    .filter(x => !!view.type ? x.type == view.type : true)
+                ) ])}
             loading={isLoading}
             columns={[ 'Type', 'Name', 'Direction', 'Traffic' ]}
             cells={x => [
                 // Date
-                x.date ?? x.type,
+                x.date ? <span className="font-bold text-lg">{x.date}</span> : x.type,
                 x.name,
                 x.direction,
-                x.traffic
+                <Size size={x.traffic}/>
             ]}
         />
     </Container>
